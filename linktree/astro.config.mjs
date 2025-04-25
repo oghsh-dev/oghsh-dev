b396f28bc30016234d2203ddf19cd6599157db02
Octavian Ghiuritan <7037696+oghsh-dev@users.noreply.github.com>
2025-04-25 18:41:34 +0300

    Update astro.config.mjs

--- linktree/astro.config.mjs
+++ linktree/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ogh.sh',
-  base: 'oghsh-dev/linktree',
+  base: 'oghsh-dev/linktree/',
 });
